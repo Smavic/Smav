6e70fa5d7e39181fae82ad2ac80cc6326c61c1b3
mustaqinIshak <mustaqinishak@gmail.com>
2020-02-06 19:38:33 +0700

    client: add style

---
+++ client/src/app.js
@@ -0,0 +1,114 @@
+$('.error').hide()
+$("#registerForm").hide()
+var $err = ""
+
+$('#loginForm').on('submit', function (event) {
+    event.preventDefault()
+    var email = $(".email").val()
+    var password = $(".password").val()
+    console.log('wooe')
+    console.log(email, password)
+    loginUser(email, password)
+})
+
+$('#registerForm').on('submit', function (event) {
+    event.preventDefault()
+    var nameRegister = $(".name").val()
+    var emailRegister = $(".emailRegister").val()
+    var passwordRegister = $(".passwordRegister").val()
+    registerUser(nameRegister, emailRegister, passwordRegister)
+   
+})
+
+$('.register-click').on('click', function(event) {
+    $('#loginForm').hide()
+    $("#registerForm").show()
+})
+
+function checkLogin(){
+    if(localStorage.getItem("token")){
+        $('.container-login').hide()
+    } else {
+        $('.container-login').show()
+    }
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        localStorage.clear()
+        checkLogin()
+        console.log('User signed out.');
+
+    });
+  }
+
+
+function registerUser(name, email, password){
+    console.log(name)
+    console.log(email)
+    console.log(password)
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/users/register',
+        data: {
+            name,
+            email,
+            password 
+        }
+    })
+    .done(response => {
+        console.log(response)
+        $('#loginRegister').hide()
+    })
+    .fail(err => {
+        console.log(err)
+        $('.error').text(err.responseJSON.errors).show()
+        setTimeout(() => {
+            $('.error').hide() 
+        }, 1000);
+    })
+}
+function loginUser(email, password) {
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/users/login',
+        data: {
+            email,
+            password 
+        }
+    })
+    .done(response => {
+        console.log(response)
+        localStorage.setItem('token', response.token)
+        checkLogin()
+    })
+    .fail(err => {
+        console.log(err)
+        $('.error').text(err.responseJSON.errors).show()
+        setTimeout(() => {
+            $('.error').hide() 
+        }, 1000);
+    })
+}
+  
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/users/googleSignIn",
+        cache: false,
+        data: {
+            googleToken: id_token
+        }
+    })
+    .done(response => {
+        console.log('berhasil login dengan google')
+        console.log(response)
+        localStorage.setItem('token', response.token)
+        checkLogin()
+    })
+    .fail(err => {
+        console.log(err)
+    })
+}
