989c0f80604d1bd8928eed05fa3c7d5fd870b040
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-07 10:03:02 +0700

    animes search

--- server/routes/animes.js
+++ server/routes/animes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const animeController = require('../controllers/animeController');
 
-router.get("/homepage", animeController.homepage)
+router.get("/homepage", animeController.homepage);
+router.get("/search", animeController.search);
 
 module.exports = router;
