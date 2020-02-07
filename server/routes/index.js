de4cde01578d651ea5a4bc8d2fce1d1f5b50f7d6
mustaqinIshak <mustaqinishak@gmail.com>
2020-02-07 09:10:39 +0700

    server: movie route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersRouter = require('./users');
+const movieRoute = require("./movie")
 const tv = require('./tv');
 
 /* GET home page. */
@@ -10,6 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.use("/users", usersRouter);
 router.use("/tv", tv);
+router.use("/movie", movieRoute)
 
 
 module.exports = router;
